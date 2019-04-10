e1e9dd18fb58e566cc2bac0fb22d9ed33015763c
feiwenli <lfw_dar@163.com>
2019-04-10 18:27:47 +0800

    print dir

--- src/test/java/com/basics/tests/TestParameter.java
+++ src/test/java/com/basics/tests/TestParameter.java
@@ -32,8 +32,10 @@ public class TestParameter {
     @BeforeTest
     public void before(String browser, String url){
         if(browser.equals("chrome")){
+            System.out.println("driverpath:"+System.getProperty("user.dir")+"test-classes/driver/chromedriver.exe");
             System.setProperty("webdriver.chrome.driver", "test-classes/driver/chromedriver.exe");
             ChromeOptions options = new ChromeOptions();
+            // 解决问题：Timed out receiving message from renderer
             options.addArguments("--disable-gpu");
             driver = new ChromeDriver(options);
         }else if(browser.equals("firefox")){
